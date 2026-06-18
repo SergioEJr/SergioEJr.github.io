08b06d758086a9a510db3631ef8f64594d6d6b16
Sergio Eraso <sergioerasojr@gmail.com>
2026-06-17 23:28:19 -0400

    visual bug fix pt 2

---
+++ src/utils/pageTransition.ts
@@ -0,0 +1,30 @@
+export const pageFade = {
+	forwards: {
+		old: {
+			name: 'siteFadeOut',
+			duration: 180,
+			easing: 'ease',
+			fillMode: 'both',
+		},
+		new: {
+			name: 'siteFadeIn',
+			duration: 180,
+			easing: 'ease',
+			fillMode: 'both',
+		},
+	},
+	backwards: {
+		old: {
+			name: 'siteFadeOut',
+			duration: 180,
+			easing: 'ease',
+			fillMode: 'both',
+		},
+		new: {
+			name: 'siteFadeIn',
+			duration: 180,
+			easing: 'ease',
+			fillMode: 'both',
+		},
+	},
+};
